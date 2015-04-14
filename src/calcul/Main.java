6bda074964b629c32a8f5b3ad0998c1656599dc2
Picouze <thomashoze@gmail.com>
2015-04-14 15:37:18 +0200

    dzqd
    dzq

---
+++ src/calcul/Main.java
@@ -0,0 +1,30 @@
+package calcul;
+
+public class Main {
+
+	private int multiplier(int nb1, int nb2){
+		int result = nb1*nb2;
+		return result;
+	}
+	
+	private int additioner(int nb1, int nb2){
+		int result = nb1+nb2;
+		return result;
+	}
+	
+	private int soustracter(int nb1, int nb2){
+		int result = nb2 - nb1;
+		return result;
+	}
+	
+	private int diviser(int nb1, int nb2){
+		int result = nb2/nb1;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
