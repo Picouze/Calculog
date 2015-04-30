149345e36b3f6a08968f62b1ea85fc20e29ae43b
DDaFox <DaFox45200@gmail.com>
2015-04-30 15:33:33 +0200

    Oublie de classe

---
+++ src/design/patterns/CalculObserver.java
@@ -0,0 +1,12 @@
+package design.patterns;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class CalculObserver implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println(arg);
+	}
+}
