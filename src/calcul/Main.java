1c59f1617daaf5a1ee9c4b271ce39b6d1c65b491
Picouze <thomashoze@gmail.com>
2015-04-30 15:43:54 +0200

    main

--- src/calcul/Main.java
+++ src/calcul/Main.java
@@ -1,45 +1,27 @@
 package calcul;
 
-import java.util.Scanner;
-
-public class Main {
-	
-	
-
-	private int multiplier(int nb1, int nb2){
-		int result = nb1*nb2;
-		return result;
-	}
-	
-	private int additioner(int nb1, int nb2){
-		int result = nb1+nb2;
-		return result;
-	}
-	
-	private int soustracter(int nb1, int nb2){
-		int result = nb2 - nb1;
-		return result;
-	}
-	
-	private int diviser(int nb1, int nb2){
-		int result = nb2/nb1;
-		return result;
-	}
-	
-	private String detecterOperande(String operation){
-		String type;
-		type = "/";
-		return type;
-	}
-	
-	private static String saisieOperation(){ 
-		Scanner scan = new Scanner(System.in);
-		return scan.toString();
-	}
-	
+import java.util.*;
+/**
+ * Classe principal
+ * @author Thomas
+ *
+ */
+public class Main {	
 	public static void main(String[] args) {
-		String operation = saisieOperation();
-		System.out.println(operation);
+		Calculator calc = new Calculator();
+		// On saisit l'opération
+		String operation = calc.saisieOperation(); 
+		//System.out.println(operation);
+		// On détecte l'opérande (+, - , * ou /)
+		String type = calc.detecterOperande(operation);
+		//System.out.println(type);
+		// On récupère les chiffres
+		List<String> nblist = new ArrayList<String>();
+		nblist = calc.decomposerOperation(operation, type);
+		int nb1 = Integer.parseInt(nblist.get(0));
+		int nb2 = Integer.parseInt(nblist.get(1));
+		//On fait le calcul
+		int resultat = calc.calculer(nb1,nb2,type);
+		System.out.println(operation+"="+resultat);
 	}
-
 }
